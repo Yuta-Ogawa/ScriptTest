e207e3c931ceccec054bd6641cd3baa74d06993a
Yuta_Ogawa <oytkt0510@gmail.com>
2019-12-07 13:17:37 +0900

    C#スクリプトの基礎
    配列の課題と発展課題を入力しました。

---
+++ Assets/Test.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Boss {
+    private int hp = 100;
+    private int mp = 53;
+    private int power = 25;
+
+    public void Attack() {
+        Debug.Log(this.power + "のダメージを与えた。");
+    }
+
+    public void Deffence(int damage) {
+        Debug.Log(damage + "のダメージを受けた。");
+        this.hp -= damage;
+    }
+
+    public void Magic(int magicconsumption) {
+        while (mp > 5) {
+            this.mp -= magicconsumption;
+            Debug.Log("魔法攻撃をした。残りのMPは" + this.mp);
+
+        }  
+            Debug.Log("MPが足りないため魔法が使えない。");
+
+    }
+}
+
+public class Test : MonoBehaviour {
+
+    void Start() {
+        int[] arrays = { 25, 49, 10, 18, 59 };
+
+        for (int s = 0; s < arrays.Length; s++) {
+            Debug.Log(arrays[s]);
+        }
+        for (int t = 4; 0 <= t; t--) {
+            Debug.Log(arrays[t]);
+        }
+    
+
+    
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+        lastboss.Deffence(3);
+        lastboss.Magic(5);
+    }
+    void Update() {
+
+    }
+}
+
